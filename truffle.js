bdc4da9319e75c51d78b88455273af4c62ae43ac
Joanna <joanna.moubarak@gmail.com>
2018-09-26 14:55:27 +0300

    second commit

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -14,8 +17,27 @@ module.exports = {
                network_id: "4224",
                //gas: 4700000,
                //from: '0x8e860611e1b4ec3359d0f72f1092480da04e889e'
-          }
+          },
 
+          rinkeby: {
+                host: "localhost",
+                port: 8545,
+                network_id: "4", // rinkeby testnet
+                //"gas":    4700000  ,
+              //  gasPrice: 20000000000,
+                //"gasPrice": 100000000000,
+                from: '0x44c4e3a3e8a4b5fc2b9b64ef0a63b7fed192cf97'
+
+           },
+
+           ropsten: {
+            provider: function () {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+            },
+            network_id: 3,
+            gas: 4500000,
+            gasPrice: 10000000000
+        }
 
      }
 };
